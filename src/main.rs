b3ac6f1154e383fff7a7363c5908f2d8877e84db
Milton <milton.depaula@featurespace.co.uk>
2023-11-23 20:17:23 +0000

    Start implementing a repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use rust_monkey::repl;
+use std::io;
+use whoami;
+
 fn main() {
-    println!("Hello, world!");
+    let user = whoami::username();
+    println!("Hello {}! This is the monkey programing language!", user);
+    println!("Feel free to type in some commands");
+    match repl::start(io::stdin(), io::stdout()) {
+        io::Result::Ok(_) => (),
+        io::Result::Err(_err) => (),
+    }
 }
