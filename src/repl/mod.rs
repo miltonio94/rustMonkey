47f46c63a5c9b344988c04cde06a2ef4bf517ba8
Milton <milton.depaula@featurespace.co.uk>
2023-11-24 11:43:37 +0000

    Add repl dir that was forgotten in last commit

---
+++ src/repl/mod.rs
@@ -0,0 +1,30 @@
+use crate::lexer;
+use crate::token;
+use std::io::Write;
+use std::io::{self, BufRead};
+
+static PROMPT: &str = ">> ";
+
+pub fn start(io_in: io::Stdin, mut io_out: io::Stdout) -> io::Result<()> {
+    loop {
+        io_out.lock();
+        io_out.write_all(PROMPT.as_bytes())?;
+        io_out.flush()?;
+
+        let mut scanner = io_in.lock();
+        let mut line = String::new();
+        scanner.read_line(&mut line)?;
+
+        let mut l = lexer::Lexer::new(line);
+
+        let mut tok = l.next_token();
+        while tok != token::Token::Eof {
+            let mut out = Vec::new();
+            write!(out, "{:?}\n", tok)?;
+            io_out.write_all(out.as_slice())?;
+            out.flush()?;
+
+            tok = l.next_token();
+        }
+    }
+}
